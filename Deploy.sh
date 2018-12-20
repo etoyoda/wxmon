bd8c80064408645829a80db185064db2de12c56b
TOYODA Eizi <toyoda.eizi@gmail.com>
2018-12-20 03:00:05 +0000

    bugfix before running; test environment

---
+++ Deploy.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+if [ -f .deploy ] ; then
+  source ./.deploy
+else
+  cat > .deploy <<CONFIG
+bindir=/nwp/bin
+priv=nwp
+cgidir=/usr/lib/cgi-bin
+CONFIG
+  echo please check config file .deploy and rerun.
+  exit 1
+fi
+: ${bindir:?} ${priv:?} ${cgidir:?}
+
+target="act-*.sh jmxscan.rb"
+
+sudo -u $priv install -m 0755 $target $bindir
+
+#sudo install -m 0755 syndl.cgi $cgidir/
--- act-jmx-00scan.sh
+++ act-jmx-00scan.sh
@@ -10,14 +10,20 @@ PATH=/bin:/usr/bin
 
 cd ${datedir}
 test ! -e tmp.ltsv || rm -f tmp.ltsv
+ymd=$(basename ${datedir} .new)
 
-rc=0 && $ruby ${script} upstream/jmx-2*.tar > tmp.ltsv || rc=$?
+if ! test jmx-${ymd}.tar -nt jmx-index-${ymd}.ltsv
+  logger --tag wxmon --id=$$ -p news.err -s -- "jmx-index-${ymd}.ltsv up to date"
+  exit 0
+fi
+
+rc=0 && $ruby ${script} jmx-2???-??-??.tar > tmp.ltsv || rc=$?
 if (( $rc != 0 )) ; then
   logger --tag wxmon --id=$$ -p news.err -s -- "jmxscan rc=$rc"
   exit $rc
 fi
 
-mv -f tmp.ltsv jmx-index.ltsv
+mv -f tmp.ltsv jmx-index-${ymd}.ltsv
 
 exit 0
 
