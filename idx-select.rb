268d46a940bfb1752a83770f3f38668b195fe1b1
TOYODA Eizi <toyoda.eizi@gmail.com>
2019-09-13 00:06:49 +0000

    bugfix: syntax error

--- idx-select.rb
+++ idx-select.rb
@@ -1,7 +1,8 @@
 require 'time'
 
-since = Time.parse(ARGV.shift or '1970-01-01T00:00:00Z')
-before = Time.parse(ARGV.shift or '2199-12-31T23:59:59Z')
+since = Time.parse(ARGV.shift || '1970-01-01T00:00:00Z')
+before = Time.parse(ARGV.shift || '2199-12-31T23:59:59Z')
+STDERR.puts [since, before].inspect
 
 for line in ARGF
   h = Hash[*line.chomp.split(/\t/).map{|c|c.split(/:/,2)}.flatten]
