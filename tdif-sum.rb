e32d60027f6ca6200d0fcaafa728850255191ee5
TOYODA Eizi <toyoda.eizi@gmail.com>
2019-09-25 00:22:54 +0000

    simplified comment editing

--- tdif-sum.rb
+++ tdif-sum.rb
@@ -1,19 +1,19 @@
 db = Hash.new(0)
-nf = n = 0
+n = 0
 for line in ARGF
   a = line.chomp.split(/\t/)
   case a.first
-  when /^#fail/ then
-    nf += a[1].to_i
-    n += a[2].to_i
+  when /^#/ then
   when /^\d/ then
     dt = a[0].to_i
     dt = (dt / 10) * 10
-    db[dt] += a[1].to_i
+    x = a[1].to_i
+    n += x
+    db[dt] += x
   end
 end
 
-puts ['#fail', nf, n, "%4.2f" % (100.0 * nf / n)].join("\t")
+puts "#n\t#{n}"
 db.keys.sort.each {|dt|
   puts [dt, db[dt]].join("\t")
 }
